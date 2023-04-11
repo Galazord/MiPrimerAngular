97197809aae4b8a112ebe838a744631be2ddf09b
Galazord <galbarack@hotmail.com>
2023-04-11 18:02:19 +0200

    commit-V1-save-1

---
+++ frontend/src/app/components/mi-componente/mi-componente.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core'
+
+// Añadir el decorador (no poner ; al final)
+@Component({
+    selector: 'mi-componente',
+    templateUrl: './mi-componente.component.html'
+})
+
+export class MiComponente {
+
+    public titulo: string;
+    public comentario: string;
+    public year: number;
+
+    constructor(){
+        this.titulo = "Hola mundo, soy MI COMPONENTE";
+        this.comentario = "Este es mi primer componente";
+        this.year = 2023;
+
+        console.log("Mi componente cargado !!", this.year);
+    }
+}
